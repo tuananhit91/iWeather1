3ddfc565319fc10f4feb89924eee105b20a101a5
Hoang Tuan Anh <tuananhit91@gmail.com>
2015-07-08 09:52:44 +0700

    Initial Commit

---
+++ iWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iWeather
+//
+//  Created by admin on 7/8/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iWeather
+//
+//  Created by admin on 7/8/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
